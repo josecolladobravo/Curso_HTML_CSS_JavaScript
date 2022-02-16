d42c42f6410eb9dcba177ff07ddc37071e3d7637
jose collado <josecolladobravo@gmail.com>
2022-02-16 21:17:52 +0100

    Subida parte de la Lección 02

--- JavaScript/Leccion02/02-01-TiposDatosJavaScript.js
+++ JavaScript/Leccion02/02-01-TiposDatosJavaScript.js
@@ -1,3 +1,66 @@
 //Tipo de dato string
-var nombre = "Jose";
+var nombre = "Mara";
 console.log(nombre);
+
+nombre = 1000;
+console.log(nombre);
+
+
+//Tipo de dato numerico
+var numero = 1000;
+console.log(numero);
+
+//Tipo de dato objeto
+var objeto = {
+    nombre : "Juan",
+    apellido : "Perez",
+    telefono : "21431431"
+}
+
+console.log(objeto);
+
+//Tipo de dato boolean
+var bandera = true;
+console.log(bandera);
+
+//Tipo de dato function
+function miFuncion(){}
+console.log(miFuncion);
+
+//Tipo de dato Symbol
+var simbolo = Symbol("mi simbolo");
+console.log(simbolo);
+
+//Tipo clase es una function
+class Persona{
+    constructor(nombre, apellido){
+        this.nombre=nombre;
+        this.apellido=apellido;
+    }
+}
+console.log(Persona);
+
+//Tipo undefined
+var x;
+console.log(x);
+
+x = undefined;
+console.log(x);
+
+
+//null = ausencia de valor
+var y = null;
+console.log(y);
+
+var autos = ['BMW', 'Audi', 'Volvo'];
+console.log(autos);
+console.log(typeof autos);
+
+var z='';
+console.log(z);
+console.log(typeof z);
+
+
+
+
+
